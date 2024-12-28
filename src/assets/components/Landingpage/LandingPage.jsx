d060453a47edcf0d864365c246235fdb810f0dfc
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2024-12-28 16:46:12 +0100

    Refactor Landingpage & ErrorPage

--- src/assets/components/Landingpage/LandingPage.jsx
+++ src/assets/components/Landingpage/LandingPage.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 export function LandingPage() {
     return(
-        <div className='landing-wrapper'>
+        <div className='landing-page'>
             <div className='landing-body'>
                 <img className='pokemon-logo' src="./pokemon-logo.png" alt="Pokémon logo" />
                 <h1>The Pokémon cards website</h1>
