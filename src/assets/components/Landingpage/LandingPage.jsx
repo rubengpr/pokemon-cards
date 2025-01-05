81a0e32348bd7c1b6229e7dfd76faa7d1258b4ac
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2025-01-06 00:12:47 +0100

    Add PokemonGrid component to LandingPage

--- src/assets/components/Landingpage/LandingPage.jsx
+++ src/assets/components/Landingpage/LandingPage.jsx
@@ -1,10 +1,12 @@
 import './LandingPage.css'
 import { Footer } from '../Footer/Footer'
 import { Link } from 'react-router-dom'
+import { PokemonGrid } from './PokemonGrid/PokemonGrid'
 
 export function LandingPage() {
     return(
         <div className='landing-page'>
+            <PokemonGrid />
             <div className='landing-body'>
                 <img className='pokemon-logo' src="./pokemon-logo.png" alt="Pokémon logo" />
                 <h1>The Pokémon cards website</h1>
@@ -12,8 +14,9 @@ export function LandingPage() {
                 <Link to="/cards">
                     <button className='primary-button'>Go to cards</button>
                 </Link>
+                <Footer />
             </div>
-            <Footer />
+            <PokemonGrid />
         </div>
     )
 }
---
+++ src/assets/components/Landingpage/PokemonGrid/PokemonGrid.jsx
@@ -0,0 +1,62 @@
+import './PokemonGrid.css';
+
+export function PokemonGrid() {
+    return(
+        <div className='pokemon-grid'>
+            <div className='pokemon-grid-item'>
+                <img className='grid-item-img' src="./public/pikachu.svg" alt="A Pokémon" />
+            </div>
+            <div className='pokemon-grid-item'></div>
+            <div className='pokemon-grid-item'></div>
+            <div className='pokemon-grid-item'></div>
+
+            <div className='pokemon-grid-item'></div>
+            <div className='pokemon-grid-item'></div>
+            <div className='pokemon-grid-item'>
+                <img className='grid-item-img' src="./public/blastoise.svg" alt="A Pokémon" />
+            </div>
+            <div className='pokemon-grid-item'></div>
+            
+            <div className='pokemon-grid-item'></div>
+            <div className='pokemon-grid-item'>
+                <img className='grid-item-img' src="./public/charizard.svg" alt="A Pokémon" />
+            </div>
+            <div className='pokemon-grid-item'></div>
+            <div className='pokemon-grid-item'></div>
+
+            <div className='pokemon-grid-item'></div>
+            <div className='pokemon-grid-item'></div>
+            <div className='pokemon-grid-item'></div>
+            <div className='pokemon-grid-item'>
+                <img className='grid-item-img' src="./public/gengar.svg" alt="A Pokémon" />
+            </div>
+            <div className='pokemon-grid-item'>
+                <img className='grid-item-img' src="./public/hypno.svg" alt="A Pokémon" />
+            </div>
+            <div className='pokemon-grid-item'></div>
+            <div className='pokemon-grid-item'></div>
+            <div className='pokemon-grid-item'></div>
+
+            <div className='pokemon-grid-item'></div>
+            <div className='pokemon-grid-item'></div>
+            <div className='pokemon-grid-item'>
+                <img className='grid-item-img' src="./public/golduck.svg" alt="A Pokémon" />
+            </div>
+            <div className='pokemon-grid-item'></div>
+            
+            <div className='pokemon-grid-item'></div>
+            <div className='pokemon-grid-item'>
+                <img className='grid-item-img' src="./public/jigglypuff.svg" alt="A Pokémon" />
+            </div>
+            <div className='pokemon-grid-item'></div>
+            <div className='pokemon-grid-item'></div>
+
+            <div className='pokemon-grid-item'></div>
+            <div className='pokemon-grid-item'></div>
+            <div className='pokemon-grid-item'></div>
+            <div className='pokemon-grid-item'>
+                <img className='grid-item-img' src="./public/kadabra.svg" alt="A Pokémon" />
+            </div>
+        </div>
+    )
+}
